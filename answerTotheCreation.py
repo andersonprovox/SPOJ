5c2e7c714c1f5d8a000edaa654e856fbeb33e1a6
Anderson da Fonseca <andersongfonseca@gmail.com>
2017-07-06 23:53:51 -0300

    Add files via upload
    First exercises resolved from SPOJ plataform http://www.spoj.com

---
+++ answerTotheCreation.py
@@ -0,0 +1,9 @@
+'''build a program based in the case of the Douglas Adams'book Answer to live, 
+the universe, and everything to the answer of the creation of 
+the word by a computer the answer is 42'''
+
+while True:
+	n = int(input())
+	print("%d" % n)
+	if n == 42:
+		break;
